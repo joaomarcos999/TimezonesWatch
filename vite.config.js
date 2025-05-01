ed469d49dac102394ce73d316c7c4e5e93736b92
João Marcos <joao_marcos99@hotmail.com>
2025-05-01 14:56:46 -0300

    Add gh-pages dependency and update Vite configuration for deployment scripts

--- vite.config.js
+++ vite.config.js
@@ -3,3 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '/TimezonesWatch/',
 });
+
+"scripts": {
+  "dev": "vite",
+  "build": "vite build",
+  "preview": "vite preview",
+  "deploy": "gh-pages -d dist"
+}
